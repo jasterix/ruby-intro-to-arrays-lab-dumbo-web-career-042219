e483960d2596caa42cce642c67027c208d06ea90
Jasterix C <loune.calixte@gmail.com>
2019-04-02 19:10:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,10 +1,10 @@
-def instantiate_new_array
-  my_new_array= [1,2,3,4,5]
-end
+# def instantiate_new_array
+#   my_new_array= [1,2,3,4,5]
+# end
 
-arr2 = Array.new
-arr3=[1,2,3,4,5,6,7]
-my_new_array[0]
-my_new_array[2]
-my_new_array[-1]
-return arr3.length
+# arr2 = Array.new
+# arr3=[1,2,3,4,5,6,7]
+# my_new_array[0]
+# my_new_array[2]
+# my_new_array[-1]
+# return arr3.length
